83b055d388744b23fbadd87943c02f3e90013a1b
Aleksey <andreev1984@bk.ru>
2024-10-13 16:55:59 +0300

    домашка1

---
+++ src/homework1/task_3/MainTask_3.java
@@ -0,0 +1,10 @@
+package homework1.task_3;
+
+public class MainTask_3 {
+    public static void main(String[] args) {
+        Answer_3 ans = new Answer_3();
+
+        int number = ans.readInt("Введите число:");
+        System.out.println("Сумма цифр числа " + number + " равна: " + ans.sumDigits(number));
+    }
+}
